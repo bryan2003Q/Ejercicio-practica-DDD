842b742f3ee3b25bc0e12f6b68e2da39a91a1243
BryanQ <quishpebryan1234@gmail.com>
2026-06-04 23:30:00 -0500

    Commit de prueba

--- backend/src/main/java/com/ejercicio/practica/modules/library/application/LibraryService.java
+++ backend/src/main/java/com/ejercicio/practica/modules/library/application/LibraryService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//ejemplo de commit 
+//ejemplo de commit parte 1
 @Service
 public class LibraryService {
 
